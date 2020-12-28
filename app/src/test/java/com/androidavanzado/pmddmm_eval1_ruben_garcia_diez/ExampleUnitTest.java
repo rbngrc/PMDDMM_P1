aa3184661d7b4a236843f7e764f4ad68065b0831
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-12-28 22:24:42 +0100

    first commit

---
+++ app/src/test/java/com/androidavanzado/pmddmm_eval1_ruben_garcia_diez/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.androidavanzado.pmddmm_eval1_ruben_garcia_diez;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
