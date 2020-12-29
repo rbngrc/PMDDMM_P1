e1209e0982789a7f1188fff5045afd5c611835f8
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-12-29 12:59:43 +0100

    Spinner options

--- app/src/main/java/com/androidavanzado/pmddmm_eval1_ruben_garcia_diez/MainActivity.java
+++ app/src/main/java/com/androidavanzado/pmddmm_eval1_ruben_garcia_diez/MainActivity.java
@@ -3,12 +3,21 @@ package com.androidavanzado.pmddmm_eval1_ruben_garcia_diez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinnerOpciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        spinnerOpciones=findViewById(R.id.spinnerOpciones);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,R.array.spinnerOpciones,android.R.layout.simple_spinner_item);
+        spinnerOpciones.setAdapter(arrayAdapter);
+
+   }
 }
