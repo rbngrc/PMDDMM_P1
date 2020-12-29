ba44fd818e54698cb14cc399bb8b8942f8b45b1c
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-12-29 13:50:03 +0100

    Message dialog added

--- app/src/main/java/com/androidavanzado/pmddmm_eval1_ruben_garcia_diez/MainActivity.java
+++ app/src/main/java/com/androidavanzado/pmddmm_eval1_ruben_garcia_diez/MainActivity.java
@@ -1,10 +1,14 @@
 package com.androidavanzado.pmddmm_eval1_ruben_garcia_diez;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this,R.array.spinnerOpciones,android.R.layout.simple_spinner_item);
         spinnerOpciones.setAdapter(arrayAdapter);
 
+        findViewById(R.id.btnReservar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cuadroDialogo();
+            }
+        });
+
+   }
+
+   private void cuadroDialogo(){
+       AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+       builder.setTitle("Titulo");
+       builder.setMessage("Se va a proceder a realizar la reserva")
+           .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+               @Override
+               public void onClick(DialogInterface dialogInterface, int i) {
+                   Toast.makeText(getApplicationContext(), "Se ha realizado la reserva de la plaza", Toast.LENGTH_LONG).show();
+               }
+           })
+           .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+               @Override
+               public void onClick(DialogInterface dialogInterface, int i) {
+                   dialogInterface.dismiss();
+               }
+           })
+               .setCancelable(false)
+               .show();
    }
 }
