a644b60bae7f710f0cac4e0cbe4218163adc5d86
Ruben Garcia <rubengarciarbngrc@gmail.com>
2020-12-29 17:06:09 +0100

    Web View and Navigation Bar Added

--- app/src/main/java/com/androidavanzado/pmddmm_eval1_ruben_garcia_diez/EverisWebActivity.java
+++ app/src/main/java/com/androidavanzado/pmddmm_eval1_ruben_garcia_diez/EverisWebActivity.java
@@ -3,12 +3,21 @@ package com.androidavanzado.pmddmm_eval1_ruben_garcia_diez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class EverisWebActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_everis_web);
+
+        webView = findViewById(R.id.webViewEveris);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.everisschool.com/");
+
     }
 }
